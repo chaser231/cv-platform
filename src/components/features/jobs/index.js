681ce389caf5909ad1118cf27739ea33829b66ac
Gary Yakovlev <gary-yakovlev@users.noreply.github.com>
2025-11-27 17:02:54 +0300

    fix: remove duplicate export of TailoredResumesList

--- src/components/features/jobs/index.js
+++ src/components/features/jobs/index.js
@@ -6,5 +6,3 @@ export { default as JobsView } from './JobsView';
 export { default as JobTailoringView } from './JobTailoringView';
 export { default as TailoredResumesList } from './TailoredResumesList';
 export { default as ResumeDetailView } from './ResumeDetailView';
-export { default as TailoredResumesList } from './TailoredResumesList';
-
